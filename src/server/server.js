240f9d54e8d15672a041895bdeeee6d2a2f5e234
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-21 15:56:50 +0100

    Move "/" route to /routes

---
+++ src/server/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import chalk from 'chalk';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import app from './app/app.js';
+
+const nodeEnv = process.env.NODE_ENV || `development`;
+console.log(chalk.blue(`Environment: ${nodeEnv}`));
+
+const port = parseInt(process.env.EXPRESS_PORT || '3000', 10);
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const server = app.listen(port, () => {
+	switch (nodeEnv) {
+		case 'production':
+		case 'build':
+			app.use(express.static(path.join(__dirname, '../client')));
+
+			app.get('*', (req, res) => {
+				res.sendFile(path.join(__dirname, '../client/index.html'));
+			});
+
+			console.log(`API accessible at ${process.env.API_URL}/`);
+			break;
+
+		case 'development':
+		default:
+			console.log(
+				chalk.blue(`API accessible at http://localhost:${port}/api`)
+			);
+			console.log(
+				chalk.blue(`React app available at http://localhost:5173/`)
+			);
+			break;
+	}
+});
+
+export default server;
