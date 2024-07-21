c251b697a17706be6215962892cb219e8167216a
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-22 00:20:40 +0100

    Delete old commented out code

--- src/server/app/middleware/auth.js
+++ src/server/app/middleware/auth.js
@@ -1,25 +1,6 @@
-import express from 'express';
-// import cors from 'cors';
 import jwt from 'jsonwebtoken';
-import cookieParser from 'cookie-parser';
-import chalk from 'chalk';
 import * as dotenv from 'dotenv';
 dotenv.config();
-
-// const app = express();
-// app.use([
-// 	express.json(),
-// 	cookieParser(),
-// 	cors({
-// 		origin:
-// 			nodeEnv === 'production'
-// 				? process.env.LIVE_CLIENT
-// 				: 'http://localhost:5173',
-// 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-// 		allowedHeaders: ['Content-Type', 'Authorization'],
-// 		credentials: true,
-// 	}),
-// ]);
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -32,9 +13,7 @@ export const verifyToken = (req, res, next) => {
 
 	jwt.verify(token, jwtSecret, (err, decoded) => {
 		if (err) {
-			return res
-				.status(401)
-				.json({ message: 'Failed to authenticate token' });
+			return res.status(401).json({ message: 'Invalid token' });
 		}
 
 		req.userId = decoded.userId;
