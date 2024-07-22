8907948f7fa7fb18cb64902eca69f9135a4d9aee
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-22 15:58:27 +0100

    Simplify server logs

--- src/server/server.js
+++ src/server/server.js
@@ -7,36 +7,36 @@ dotenv.config();
 
 import app from './app/app.js';
 
-const nodeEnv = process.env.VITE_NODE_ENV || `development`;
-console.log(chalk.blue(`Environment: ${nodeEnv}`));
+const nodeEnv = process.env.VITE_NODE_ENV || `production`;
+console.log(
+	'Node environment:',
+	process.env.VITE_NODE_ENV || chalk.yellow('VITE_NODE_ENV not set!')
+);
 
 const port = parseInt(process.env.EXPRESS_PORT || '3000', 10);
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const server = app.listen(port, () => {
-	switch (nodeEnv) {
-		case 'production':
-		case 'build':
-			app.use(express.static(path.join(__dirname, '../client')));
+	const isProdOrBuild = ['production', 'build'].includes(nodeEnv);
+	const baseUrl =
+		nodeEnv === 'production'
+			? process.env.DEPLOYED_SITE_URL
+			: `http://localhost:${port}`;
 
-			app.get('*', (req, res) => {
-				res.sendFile(path.join(__dirname, '../client/index.html'));
-			});
-
-			console.log(`API accessible at ${process.env.API_URL}/`);
-			break;
-
-		case 'development':
-		default:
-			console.log(
-				chalk.blue(`API accessible at http://localhost:${port}/api`)
-			);
-			console.log(
-				chalk.blue(`React app available at http://localhost:5173/`)
-			);
-			break;
+	if (isProdOrBuild) {
+		app.use(express.static(path.join(__dirname, '../client')));
+		app.get('*', (req, res) => {
+			res.sendFile(path.join(__dirname, '../client/index.html'));
+		});
 	}
+
+	const frontEndUrl = isProdOrBuild ? baseUrl : 'http://localhost:5173';
+
+	console.log(
+		chalk.blue(`API accessible at ${baseUrl}/api\n`),
+		chalk.blue(`Front-end accessible at ${frontEndUrl}`)
+	);
 });
 
 export default server;
