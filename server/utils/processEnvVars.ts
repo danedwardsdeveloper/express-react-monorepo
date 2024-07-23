feabe1d279d223784060b2d52b443062a6d492b8
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-23 18:43:30 +0100

    Simplify logic

--- server/utils/processEnvVars.ts
+++ server/utils/processEnvVars.ts
@@ -2,36 +2,44 @@ import chalk from 'chalk';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-function checkAndLogEnvVariable(variable: string): string {
+function processVariable(variable: string): string {
     const value = process.env[variable];
+    if (!value) throw new Error(chalk.red(`${variable} not set`));
 
-    if (value == null) {
-        console.error(chalk.red(`${variable} not set`));
-        return '';
-    } else {
-        console.log(`${variable}: ${chalk.blue(value)}`);
-        return value;
-    }
+    const isSecret = variable.toLowerCase().includes('secret');
+    console.log(`${variable}: ${chalk.blue(isSecret ? `${value.slice(0, 6)}...` : value)}`);
+    return value;
 }
 
-function calculateAndLogAllowedOrigins(variable: string): string[] | null {
-    const value = process.env[variable];
+function processAllowedOrigins(origins: string, environment: string): string[] {
+    const value = process.env[origins];
 
     if (value == null) {
-        console.error(chalk.red(`EXPRESS_ALLOWED_ORIGINS not set`));
-        return null;
-    } else {
-        const originArray = value.split(',').map(origin => origin.trim());
-        console.log('Allowed origins:');
-        originArray.forEach(origin => console.log(chalk.green(`- ${origin}`)));
-        return originArray;
+        throw new Error(chalk.red(`${origins} not set`));
     }
+
+    console.log('Allowed origins:');
+
+    const originArray = value.split(',').map(origin => origin.trim());
+
+    const inProduction: boolean = environment === 'production';
+
+    originArray.forEach(origin => console.log(
+        chalk.blue(`- ${inProduction ? origin : chalk.strikethrough(origin)}`)
+    ));
+
+    !inProduction && console.log(chalk.blue(`- *`));
+
+    return inProduction ? originArray : ["*"];
 }
 
-export const expressEnv = checkAndLogEnvVariable('EXPRESS_ENV');
-export const port: number = parseInt(checkAndLogEnvVariable("EXPRESS_PORT"), 10);
-export const jwtSecret = checkAndLogEnvVariable('EXPRESS_JWT_SECRET');
 
+// Strings
+export const expressEnv = processVariable('EXPRESS_ENV');
+export const jwtSecret = processVariable('EXPRESS_JWT_SECRET');
 
+// Numbers
+export const port: number = parseInt(processVariable("EXPRESS_PORT"), 10);
 
-export const allowedOrigins = calculateAndLogAllowedOrigins("EXPRESS_ALLOWED_ORIGINS");
+// Arrays
+export const allowedOrigins = processAllowedOrigins("EXPRESS_ALLOWED_ORIGINS", expressEnv);
