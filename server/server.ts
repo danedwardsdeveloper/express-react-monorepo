ed5c211557f593158ea84e7b57c3f846928b720a
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-24 00:46:40 +0100

    Fix incorrect paths

--- server/server.ts
+++ server/server.ts
@@ -5,31 +5,33 @@ import { fileURLToPath } from 'url';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { expressEnv, port } from './utils/processEnvVars';
-import app from './app/app';
+import { expressEnv, port } from './utils/processEnvVars.js';
+import app from './app/app.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const staticPath = path.join(__dirname, '..', 'client');
 
 const server = app.listen(port, () => {
 	const isProdOrBuild = ['production', 'build'].includes(expressEnv);
+
+	if (isProdOrBuild) {
+		console.log(chalk.green(`Static files served from: ${staticPath}`));
+
+		app.use(express.static(path.join(__dirname, '../client/')));
+		app.get('*', (req, res) => {
+			res.sendFile(path.join(__dirname, '../client/index.html'));
+		});
+	}
+
 	const baseUrl =
 		expressEnv === 'production'
 			? process.env.VITE_DEPLOYED_SITE_URL
 			: `http://localhost:${port}`;
 
-	if (isProdOrBuild) {
-		app.use(express.static(path.join(__dirname, '../dist/client/')));
-		app.get('*', (req, res) => {
-			res.sendFile(path.join(__dirname, '../dist/client/index.html'));
-		});
-	}
-
 	const frontEndUrl = isProdOrBuild ? baseUrl : 'http://localhost:5173';
 
-	console.log(
-		chalk.blue(`API accessible at ${baseUrl}/api\n`),
-		chalk.blue(`Front-end accessible at ${frontEndUrl}`)
-	);
+	console.log(chalk.green(`API accessible at ${baseUrl}/api`));
+	console.log(chalk.green(`Front-end accessible at ${frontEndUrl}`));
 });
 
 export default server;
