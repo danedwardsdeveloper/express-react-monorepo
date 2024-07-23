c5d0318f3013caa4e3fd33bda3282ebf4a19f799
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-24 00:46:07 +0100

    Move allowedOrigins logic to ext module

--- server/app/middleware/middleware.ts
+++ server/app/middleware/middleware.ts
@@ -1,21 +1,18 @@
-import express from 'express';
+import express, { Express } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import dotenv from 'dotenv';
-dotenv.config();
 
-const setupMiddleware = (app) => {
+import { allowedOrigins } from '../../utils/processEnvVars.js';
+
+
+
+const setupMiddleware = (app: Express) => {
 	app.use(express.json());
 	app.use(cookieParser());
 
-	const allowedOrigin =
-		process.env.VITE_NODE_ENV === 'production'
-			? `${process.env.LIVE_CLIENT}/api`
-			: 'http://localhost:5173';
-
 	app.use(
 		cors({
-			origin: allowedOrigin,
+			origin: allowedOrigins,
 			methods: ['GET', 'POST'],
 			allowedHeaders: ['Content-Type', 'Authorization'],
 			credentials: true,
