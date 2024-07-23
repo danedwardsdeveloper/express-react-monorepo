d460b5984cc4cf13668194598e57ce65604c5b25
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-24 00:46:15 +0100

    Add types

--- server/app/routes/protected.ts
+++ server/app/routes/protected.ts
@@ -1,15 +1,19 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { verifyToken } from '../middleware/auth.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+interface CustomRequest extends Request {
+	userId?: string;
+}
+
 const protectedRouter = express.Router();
 
-protectedRouter.get('/protected', verifyToken, (req, res) => {
+protectedRouter.get('/protected', verifyToken, (req: CustomRequest, res: Response) => {
 	res.json({ message: 'This is a protected route', userId: req.userId });
 });
 
-protectedRouter.post('/logout', (req, res) => {
+protectedRouter.post('/logout', (req: Request, res: Response) => {
 	res.clearCookie('token', {
 		httpOnly: true,
 		secure: true,
