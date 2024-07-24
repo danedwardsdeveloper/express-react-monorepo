6a24440da58876f009838c7e62993a2e7f8b5018
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-24 12:20:52 +0100

    Move URL console logs to environmentChecks

--- server/server.ts
+++ server/server.ts
@@ -5,33 +5,20 @@ import { fileURLToPath } from 'url';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { expressEnv, port } from './utils/processEnvVars.js';
+import { expressEnv, port } from './utils/environmentChecks.js';
 import app from './app/app.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const staticPath = path.join(__dirname, '..', 'client');
 
 const server = app.listen(port, () => {
-	const isProdOrBuild = ['production', 'build'].includes(expressEnv);
+	const serveReactWithExpress: boolean = expressEnv !== 'development';
 
-	if (isProdOrBuild) {
-		console.log(chalk.green(`Static files served from: ${staticPath}`));
-
+	if (serveReactWithExpress) {
 		app.use(express.static(path.join(__dirname, '../client/')));
 		app.get('*', (req, res) => {
 			res.sendFile(path.join(__dirname, '../client/index.html'));
 		});
 	}
-
-	const baseUrl =
-		expressEnv === 'production'
-			? process.env.VITE_DEPLOYED_SITE_URL
-			: `http://localhost:${port}`;
-
-	const frontEndUrl = isProdOrBuild ? baseUrl : 'http://localhost:5173';
-
-	console.log(chalk.green(`API accessible at ${baseUrl}/api`));
-	console.log(chalk.green(`Front-end accessible at ${frontEndUrl}`));
 });
 
 export default server;
