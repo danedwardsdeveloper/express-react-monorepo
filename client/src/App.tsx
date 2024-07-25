5ab563ff3becbf9bc847166bc7bea3deaf6cc8c3
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-25 23:02:56 +0100

    Move global auth state to main.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,14 @@
-import { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-
-import { validateToken } from './services/AuthService';
 
 import MenuBar from './app/components/MenuBar';
 import Footer from './app/components/Footer';
 
 export default function App() {
-	const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-	useEffect(() => {
-		const checkAuth = async () => {
-			const isValid = await validateToken();
-			setIsAuthenticated(isValid);
-		};
-		checkAuth();
-	}, []);
-
 	return (
-		<div>
-			<MenuBar isAuthenticated={isAuthenticated} />
+		<>
+			<MenuBar />
 			<Outlet />
 			<Footer />
-		</div>
+		</>
 	);
 }
