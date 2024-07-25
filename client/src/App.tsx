349304bacd26e901e27d8433e5382cb6a613312a
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-25 22:03:15 +0100

    Write authentication logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,27 @@
-import { Outlet } from 'react-router-dom'
-import MenuBar from './components/MenuBar'
-import Footer from './components/Footer'
+import { useState, useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>
-      <MenuBar />
-      <Outlet />
-      <Footer />
-    </div>
-  );
+import { validateToken } from './services/AuthService';
+
+import MenuBar from './app/components/MenuBar';
+import Footer from './app/components/Footer';
+
+export default function App() {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	useEffect(() => {
+		const checkAuth = async () => {
+			const isValid = await validateToken();
+			setIsAuthenticated(isValid);
+		};
+		checkAuth();
+	}, []);
+
+	return (
+		<div>
+			<MenuBar isAuthenticated={isAuthenticated} />
+			<Outlet />
+			<Footer />
+		</div>
+	);
 }
-
-export default App;
