288505c25c94e1eed55db70687e6da676c2ed14d
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-25 22:08:02 +0100

    Set up router properly

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,29 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	RouterProvider,
+	Route,
+} from 'react-router-dom';
 
-import App from './App'
-import Home from './components/Home'
-import CreateAccount from './components/CreateAccount'
-import SignIn from './components/SignIn'
-import SignOut from './components/SignOut'
+import App from './App';
+import Home from './app/components/Home';
+import Protected from './app/components/Protected';
+import SignIn from './app/components/SignIn';
+import SignOut from './app/components/SignOut';
 
-import './index.tailwind.css'
+import './index.tailwind.css';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-      <Route path="/" element={<App />} >
-        <Route index element={<Home />} />
-        <Route path="create-account" element={<CreateAccount />} />
-        <Route path="sign-in" element={<SignIn />} />
-        <Route path="sign-out" element={<SignOut />} />
-      </Route>
-    )
-  )
+	createRoutesFromElements(
+		<Route path="/" element={<App />}>
+			<Route index element={<Home />} />
+			<Route path="protected" element={<Protected />} />
+			<Route path="sign-in" element={<SignIn />} />
+			<Route path="sign-out" element={<SignOut />} />
+		</Route>
+	)
+);
 
-  ReactDOM.createRoot(document.getElementById("root")! as HTMLElement).render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
-  
+ReactDOM.createRoot(document.getElementById('root')! as HTMLElement).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
