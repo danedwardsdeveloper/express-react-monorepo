da96be5127e184180f2ee85560797763a796ea8f
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-26 09:25:31 +0100

    Remove comments

--- server/app/app.ts
+++ server/app/app.ts
@@ -4,22 +4,8 @@ dotenv.config();
 
 const app = express();
 
-
-// import { setupBodyParser } from './middleware/bodyParser.js';
-// import { setupSecurityHeaders } from './middleware/securityHeaders.js';
-// import { setupRateLimiter } from './middleware/rateLimiter.js';
-// import { requestLogger } from './middleware/requestLogger.js';
-// import { errorHandler } from './middleware/errorHandler.js';
-
 import setupMiddleware from './middleware/middleware.js';
 setupMiddleware(app);
-
-// setupBodyParser(app);
-// setupSecurityHeaders(app);
-// setupRateLimiter(app);
-// app.use(requestLogger);
-
-// app.use(errorHandler);
 
 import publicRouter from './routes/public.js';
 app.use('/api', publicRouter);
